006f27e2608b6ad57ec7807a3eeb35f4f73d733c
Kiruthi124 <41139749+Kiruthi124@users.noreply.github.com>
2018-07-14 06:47:17 -0400

    Initial commit

---
+++ Atom_Files/ex1.rb
@@ -0,0 +1,7 @@
+puts "Hello World!"
+puts "Hello Again"
+puts " I like typing this."
+puts "This ids fun."
+puts "Yay ! Printing."
+puts "I'd much rather than you  'not' ."
+puts 'I "said" do not touch this.'
